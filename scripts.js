57eac8c742532ec7b22909e33232032280eaafa1
Samantha Maclaren <maclares@pbcc.edu>
2016-06-09 18:54:26 -0400

    #025: Change main image to full screen.

--- scripts.js
+++ scripts.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
   $("#About").click(function(){
     $('html, body').animate({
       scrollTop: $('.about').offset().top
-    }, 500);
+    }, 900);
   });
   $("#Portfolio").click(function(){
     $('html, body').animate({
       scrollTop: $('.portfolio').offset().top
-    }, 500);
+    }, 900);
   });
   $("#Contact").click(function(){
     $('html, body').animate({
       scrollTop: $('.contact').offset().top
-    }, 500);
+    }, 900);
   });
 });
